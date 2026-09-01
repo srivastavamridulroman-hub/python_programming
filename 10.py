c764c6c3da97a60c1564073a00cbe6dcc539bdf5
Mridul Srivastava <srivastavamridulroman@gmail.com>
2026-09-01 20:11:39 +0530

    Buy and sell

---
+++ 10.py
@@ -0,0 +1,12 @@
+# buy and sell
+class Solution(object):
+    def maxProfit(self, prices):
+        max_profit = 0
+        min_price = float("inf")
+        n = len(prices)
+
+        for i in range(0, n):
+            min_price = min(min_price, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price)
+
+        return max_profit
